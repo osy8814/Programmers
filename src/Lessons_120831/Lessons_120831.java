0f6a61140c0c6ce65cb1fa06f8e8e4fdf36835b3
haribo14 <edcosy@gmail.com>
2022-12-21 18:16:28 +0900

    짝수의 합

---
+++ src/Lessons_120831/Lessons_120831.java
@@ -0,0 +1,19 @@
+package Lessons_120831;
+
+public class Lessons_120831 {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.solution(10));
+	}
+
+}
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 0; i <= n; i++) {
+			answer = i%2==0 ? answer+=i : answer; 
+        }
+        return answer;
+    }
+}
