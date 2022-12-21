575d726e78501aae7e59b9abf779504ec47d91fe
haribo14 <edcosy@gmail.com>
2022-12-21 12:53:07 +0900

    삼각형의 완성조건(1)

---
+++ src/Lessons_120889/Lessons_120889.java
@@ -0,0 +1,21 @@
+package Lessons_120889;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class Lessons_120889 {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int[]sides= {1, 2, 3};
+		System.out.println(solution.solution(sides));
+	}
+
+}
+class Solution {
+    public int solution(int[] sides) {
+        int MaxSides = Arrays.stream(sides).max().getAsInt();
+        int OthersSum = Arrays.stream(sides).sum()-MaxSides;
+        return MaxSides>=OthersSum ? 2 : 1;
+    }
+}
