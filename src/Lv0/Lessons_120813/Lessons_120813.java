4132f75d27c591c7a17df62af396b475596aaed4
haribo14 <edcosy@gmail.com>
2022-12-22 19:53:04 +0900

    짝수가 싫어요

---
+++ src/Lv0/Lessons_120813/Lessons_120813.java
@@ -0,0 +1,25 @@
+package Lv0.Lessons_120813;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lessons_120813 {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.solution(10));
+		
+	}
+
+}
+class Solution {
+    public List<Integer> solution(int n) {
+    	List<Integer>list = new ArrayList<>();
+    	for (int i = 1; i <= n; i++) {
+			if(i%2!=0) {
+				list.add(i);
+			}
+		}
+    	return list; 
+    }
+}
