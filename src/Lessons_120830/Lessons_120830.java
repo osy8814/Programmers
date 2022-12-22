f93194b4e04993cc7742afd39071698bb56386d4
haribo14 <edcosy@gmail.com>
2022-12-22 15:20:37 +0900

    양꼬치

---
+++ src/Lessons_120830/Lessons_120830.java
@@ -0,0 +1,15 @@
+package Lessons_120830;
+
+public class Lessons_120830 {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.solution(10, 3));
+	}
+
+}
+class Solution {
+    public int solution(int n, int k) {
+    	return n*12000+(k-(n/10))*2000;
+    }
+}
