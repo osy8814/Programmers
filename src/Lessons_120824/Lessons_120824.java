553a4e39c8fd9fba0ca4757bb29f895c0505fe7d
haribo14 <edcosy@gmail.com>
2022-12-22 18:02:09 +0900

    짝수와 홀수 개수

--- src/Lessons_120824/Lessons_120824.java
+++ src/Lessons_120824/Lessons_120824.java
@@ -6,22 +6,22 @@ public class Lessons_120824 {
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		int[]num_list= {1, 2, 3, 4, 5};
+		int[] num_list = { 1, 2, 3, 4, 5 };
 		System.out.println(Arrays.toString(solution.solution(num_list)));
 	}
 
 }
+
 class Solution {
-    public int[] solution(int[] num_list) {
-    	int[] result = {0,0};
-    	for (int i : num_list) {
-			if(i%2==0) {
-				result[1]++;
-			}else {
+	public int[] solution(int[] num_list) {
+		int[] result = { 0, 0 };
+		for (int i : num_list) {
+			if (i % 2 == 0) {
 				result[0]++;
+			} else {
+				result[1]++;
 			}
 		}
-    	
-    	return result;
-    }
+		return result;
+	}
 }
