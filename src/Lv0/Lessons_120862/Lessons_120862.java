91468c636ee4ad509ff2523fb5ec9671c1becce0
haribo14 <edcosy@gmail.com>
2022-12-23 18:28:37 +0900

    최댓값 만들기(2)

---
+++ src/Lv0/Lessons_120862/Lessons_120862.java
@@ -0,0 +1,20 @@
+package Lv0.Lessons_120862;
+
+import java.util.Arrays;
+
+public class Lessons_120862 {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int[] numbers = {0, -31, 24, 10, 1, 9};
+		System.out.println(solution.solution(numbers));
+	}
+
+}
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers[0]*numbers[1]>numbers[numbers.length-2]*numbers[numbers.length-1] ? numbers[0]*numbers[1] : numbers[numbers.length-2]*numbers[numbers.length-1] ;
+    }
+}
