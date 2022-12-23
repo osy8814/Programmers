bf8aa7919a16b7b336c72b753a6b86c71d7f1f8c
haribo14 <edcosy@gmail.com>
2022-12-23 15:57:15 +0900

    문자열 정렬하기(1)

--- src/Lv0/Lessons_120850/Lessons_120850.java
+++ src/Lv0/Lessons_120850/Lessons_120850.java
@@ -2,7 +2,7 @@ package Lv0.Lessons_120850;
 
 import java.util.Arrays;
 
-public class Lessons_120550 {
+public class Lessons_120850 {
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
@@ -13,8 +13,10 @@ public class Lessons_120550 {
 class Solution {
     public int[] solution(String my_string) {
     	my_string = my_string.replaceAll("[a-z]", ""); 
-        String[] arr = my_string.split("");
+        
+    	String[] arr = my_string.split("");
         int[]answer = new int[arr.length];
+        
         for (int i = 0; i < arr.length; i++) {
         	answer[i] = Integer.parseInt(arr[i]);
 		}
