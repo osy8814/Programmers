7e8f6b5af396ec25c7090d454404b1e73ce45c45
haribo14 <edcosy@gmail.com>
2022-12-23 15:52:46 +0900

    문자열 정렬하기(1)

---
+++ src/Lv0/Lessons_120850/Lessons_120550.java
@@ -0,0 +1,25 @@
+package Lv0.Lessons_120850;
+
+import java.util.Arrays;
+
+public class Lessons_120550 {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(Arrays.toString(solution.solution("hi12392")));
+	}
+
+}
+class Solution {
+    public int[] solution(String my_string) {
+    	my_string = my_string.replaceAll("[a-z]", ""); 
+        String[] arr = my_string.split("");
+        int[]answer = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+        	answer[i] = Integer.parseInt(arr[i]);
+		}
+        Arrays.sort(answer);
+        
+    	return answer;
+    }
+}
