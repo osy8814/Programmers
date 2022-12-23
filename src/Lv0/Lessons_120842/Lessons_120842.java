f279b4016baf8c11b38fde21cb9f681c3cda9382
haribo14 <edcosy@gmail.com>
2022-12-23 23:44:52 +0900

    2차원 배열만들기

--- src/Lv0/Lessons_120842/Lessons_120842.java
+++ src/Lv0/Lessons_120842/Lessons_120842.java
@@ -6,26 +6,32 @@ public class Lessons_120842 {
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		int[] num_list = {100, 95, 2, 4, 5, 6, 18, 33, 948}; 
-		System.out.println(Arrays.deepToString(solution.solution(num_list, 3)));
-		
+		System.out.println(Arrays.toString(solution.solution(1, 2, 1, 2)));
+
 	}
 
 }
+
 class Solution {
-	public int[][] solution(int[] num_list, int n) {
-        
-		int[][] answer = new int[num_list.length/n][n];
-        int count = 0;
-		for (int i = 0; i < num_list.length/n; i++) {
-        	for (int j = 0; j < n; j++) {
-				answer[i][j] = num_list[count];
-				count++;
+	public int[] solution(int denum1, int num1, int denum2, int num2) {
+
+		int[] answer = new int[2];
+
+		answer[0] = denum1 * num2 + denum2 * num1;
+		answer[1] = num1 * num2;
+
+		System.out.println(Arrays.toString(answer));
 				
+		int min = Math.min(answer[0], answer[1]);
+
+		for (int i = min; i >= 1; i--) {
+			if (answer[0] % i == 0 && answer[1] % i == 0) {
+				System.out.println(i);
+				
+				answer[0] = answer[0] / i;
+				answer[1] = answer[1] / i;
 			}
-			
 		}
-        
-        return answer;
-    }
+		return answer;
+	}
 }
