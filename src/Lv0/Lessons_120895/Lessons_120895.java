a51fce346a3c0db40cd254485e7db9124cb0da10
haribo14 <edcosy@gmail.com>
2022-12-23 19:30:16 +0900

    인덱스 바꾸기

--- src/Lv0/Lessons_120895/Lessons_120895.java
+++ src/Lv0/Lessons_120895/Lessons_120895.java
@@ -10,7 +10,9 @@ public class Lessons_120895 {
 }
 class Solution {
     public String solution(String my_string, int num1, int num2) {
+    	
     	StringBuffer sb = new StringBuffer(my_string);
+    	
     	char num1Ch = my_string.charAt(num1); 
     	char num2Ch = my_string.charAt(num2); 
     	
