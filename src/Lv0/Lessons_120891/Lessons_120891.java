29fac234b5860f35b5581e37f3a648c929990e74
haribo14 <edcosy@gmail.com>
2022-12-23 19:30:23 +0900

    369게임

---
+++ src/Lv0/Lessons_120891/Lessons_120891.java
@@ -0,0 +1,18 @@
+package Lv0.Lessons_120891;
+
+import java.util.Arrays;
+
+public class Lessons_120891 {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.solution(369));
+	}
+
+}
+
+class Solution {
+    public int solution(int order) {
+        return (int) Arrays.stream(String.valueOf(order).split("")).filter(x->Integer.parseInt(x)%3==0).count();
+    }
+}
